5b7a2a099785c5c523ecc944a37fe5a49f8dabd0
4x6hDcMtCji9A9 <fiuc0679@gmail.com>
2026-07-08 01:26:21 -0300

    Delete start.sh

--- start.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-clear
-echo "🧍 Iniciando Bot ESTÁTUA - Versão Final"
-echo "⏰ Auto /survival: 03:30 e 15:30"
-echo "⌨️ Comando personalizado | 🗑️ Limpar tudo"
-echo "════════════════════════════════════════"
-cd ~/multi-bot-panel-certo && node server.js
